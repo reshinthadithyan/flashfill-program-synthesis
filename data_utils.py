30e8841365fa44e85e7a9b41693d9d89e9056704
Reshinth Adithyan <36307201+reshinthadithyan@users.noreply.github.com>
2021-11-07 01:26:32 +0530

    formatting code and beautifying it

--- data_utils.py
+++ data_utils.py
@@ -1,13 +1,13 @@
 import os
 import re
-from torch.utils.data import Dataset,random_split
+from torch.utils.data import Dataset, random_split
 from tqdm import tqdm
 from transformers import RobertaTokenizerFast
 from torch import Generator
 
 
 class FlashFillDataset(Dataset):
-    def __init__(self,dataset_idt,tokenizer,target_length):
+    def __init__(self, dataset_idt, tokenizer, target_length):
         """
         FlashFill torch.utils.data.Dataset object
         args:
@@ -20,18 +20,21 @@ class FlashFillDataset(Dataset):
         self.dataset_idt = dataset_idt
         self.target_length = target_length
         self.dataset_list = self.read_and_process()
-    def clean_flash_fill_data(self,data):
+
+    def clean_flash_fill_data(self, data):
         """
         Function to clean and pre-process the datapoint suitable for CausalLM
         """
-        #TOOD(reshinth) : Clean multiple \n to one
-        data = re.sub(r'\n+', '\n', data).strip()
+        # TOOD(reshinth) : Clean multiple \n to one
+        data = re.sub(r"\n+", "\n", data).strip()
         data_split_list = data.split("\n(declare-var")
         to_generate = data_split_list[0]
-        init_context = "\n(declar-var"+"\n(declar-var".join(data_split_list[1:])
-        return init_context,to_generate
+        init_context = "\n(declar-var" + "\n(declar-var".join(data_split_list[1:])
+        return init_context, to_generate
 
-    def read_and_process(self,path=os.path.join(".","DeepSynth","flashfill_dataset")):
+    def read_and_process(
+        self, path=os.path.join(".", "DeepSynth", "flashfill_dataset")
+    ):
         """
         read and process files into datapoints
         args:
@@ -39,13 +42,25 @@ class FlashFillDataset(Dataset):
         """
         dataset_list = []
         if self.dataset_idt == "FlashFill":
-            file_names = [os.path.join(path,file_name) for file_name in os.listdir(path)]
+            file_names = [
+                os.path.join(path, file_name) for file_name in os.listdir(path)
+            ]
             for file in tqdm(file_names):
-                with open(file,"r") as f:
-                    init_context,to_generate = self.clean_flash_fill_data(f.read())
-                    inputs = init_context + "<generate-flashfill>" +to_generate
-                    model_inputs = self.tokenizer(inputs, max_length=self.target_length, padding="max_length", truncation=True)
-                    labels = self.tokenizer(to_generate, max_length=self.target_length, padding="max_length", truncation=True).input_ids
+                with open(file, "r") as f:
+                    init_context, to_generate = self.clean_flash_fill_data(f.read())
+                    inputs = init_context + "<generate-flashfill>" + to_generate
+                    model_inputs = self.tokenizer(
+                        inputs,
+                        max_length=self.target_length,
+                        padding="max_length",
+                        truncation=True,
+                    )
+                    labels = self.tokenizer(
+                        to_generate,
+                        max_length=self.target_length,
+                        padding="max_length",
+                        truncation=True,
+                    ).input_ids
                     labels_example = [label if label != 0 else -100 for label in labels]
                     model_inputs["labels"] = labels_example
                     dataset_list.append(model_inputs)
@@ -55,18 +70,28 @@ class FlashFillDataset(Dataset):
 
     def __len__(self):
         return len(self.dataset_list)
+
     def __getitem__(self, index):
         return self.dataset_list[index]
 
-def train_test_valid_split(dataset_idt,tokenizer,max_length):
-    flashfill_dataset = FlashFillDataset(dataset_idt,tokenizer,max_length)
-    return random_split(flashfill_dataset,[92,8,8], generator=Generator().manual_seed(42))
+
+def train_test_valid_split(dataset_idt, tokenizer, max_length):
+    flashfill_dataset = FlashFillDataset(dataset_idt, tokenizer, max_length)
+    return random_split(
+        flashfill_dataset, [92, 8, 8], generator=Generator().manual_seed(42)
+    )
+
 
 if __name__ == "__main__":
     from torch import Generator
     from torch.utils.data import random_split
+
     trial_tokenizer = RobertaTokenizerFast.from_pretrained("Salesforce/codet5-small")
-    flashfill_dataset = FlashFillDataset("FlashFill",trial_tokenizer,1024)
+    flashfill_dataset = FlashFillDataset("FlashFill", trial_tokenizer, 1024)
     dataset_length = len(flashfill_dataset)
-    print(random_split(flashfill_dataset,[92,8,8], generator=Generator().manual_seed(42)))
+    print(
+        random_split(
+            flashfill_dataset, [92, 8, 8], generator=Generator().manual_seed(42)
+        )
+    )
     print(flashfill_dataset[-1])
--- flashfillT5.py
+++ flashfillT5.py
@@ -1,47 +1,59 @@
 import argparse
-from torch.utils.data import random_split,DataLoader
+from torch.utils.data import random_split, DataLoader
 from torch import Generator
-from transformers import T5ForConditionalGeneration, AdamW, get_linear_schedule_with_warmup, RobertaTokenizerFast
+from transformers import (
+    T5ForConditionalGeneration,
+    AdamW,
+    get_linear_schedule_with_warmup,
+    RobertaTokenizerFast,
+)
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
 import os
 from data_utils import train_test_valid_split
+
 parser = argparse.ArgumentParser()
 
-parser.add_argument("run_name",default="codet5-finetune-program-synthesis")
-parser.add_argument("dir",default="flash_fill_hash")
-parser.add_argument("model_name",default="Salesforce/codet5-small")
-parser.add_argument("dataset_idt",default="FlashFill")
-parser.add_argument("max_epochs",default=15)
-parser.add_argument("warmup_steps",default=1000)
-parser.add_argument("target_max_length",default=1024)
+parser.add_argument("run_name", default="codet5-finetune-program-synthesis")
+parser.add_argument("dir", default="flash_fill_hash")
+parser.add_argument("model_name", default="Salesforce/codet5-small")
+parser.add_argument("dataset_idt", default="FlashFill")
+parser.add_argument("max_epochs", default=15)
+parser.add_argument("warmup_steps", default=1000)
+parser.add_argument("target_max_length", default=1024)
 
 args = parser.parse_args()
 
 
 class CodeT5(pl.LightningModule):
-    #Borrowed from the awesome https://github/NielsRogge/Transformers-Tutorials by Niels Rogge
-    def __init__(self, lr=5e-5, num_train_epochs=args.max_epochs, warmup_steps=args.warmup_steps):
+    # Borrowed from the awesome https://github/NielsRogge/Transformers-Tutorials by Niels Rogge
+    def __init__(
+        self, lr=5e-5, num_train_epochs=args.max_epochs, warmup_steps=args.warmup_steps
+    ):
         super().__init__()
         self.model = T5ForConditionalGeneration.from_pretrained(args.model_name)
         tokenizer = RobertaTokenizerFast.from_pretrained(args.model_name)
-        self.trainset,self.validset,self.testset = train_test_valid_split("FlashFill",tokenizer,args.target_max_length)
+        self.trainset, self.validset, self.testset = train_test_valid_split(
+            "FlashFill", tokenizer, args.target_max_length
+        )
         self.save_hyperparameters()
 
-    def forward(self, input_ids, attention_mask, labels=None):     
-        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+    def forward(self, input_ids, attention_mask, labels=None):
+        outputs = self.model(
+            input_ids=input_ids, attention_mask=attention_mask, labels=labels
+        )
         return outputs
-    
+
     def common_step(self, batch, batch_idx):
         outputs = self(**batch)
         loss = outputs.loss
 
         return loss
-      
+
     def training_step(self, batch, batch_idx):
-        loss = self.common_step(batch, batch_idx)     
+        loss = self.common_step(batch, batch_idx)
         # logs metrics for each training_step,
         # and the average across the epoch
         self.log("training_loss", loss)
@@ -49,13 +61,13 @@ class CodeT5(pl.LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss = self.common_step(batch, batch_idx)     
+        loss = self.common_step(batch, batch_idx)
         self.log("validation_loss", loss, on_epoch=True)
 
         return loss
 
     def test_step(self, batch, batch_idx):
-        loss = self.common_step(batch, batch_idx)     
+        loss = self.common_step(batch, batch_idx)
 
         return loss
 
@@ -63,40 +75,45 @@ class CodeT5(pl.LightningModule):
         # create optimizer
         optimizer = AdamW(self.parameters(), lr=self.hparams.lr)
         # create learning rate scheduler
-        num_train_optimization_steps = self.hparams.num_train_epochs * len(self.train_dataloader()) #Quick Dirty Fix
-        lr_scheduler = {'scheduler': get_linear_schedule_with_warmup(optimizer,
-                                                    num_warmup_steps=self.hparams.warmup_steps,
-                                                    num_training_steps=num_train_optimization_steps),
-                        'name': 'learning_rate',
-                        'interval':'step',
-                        'frequency': 1}
-        
+        num_train_optimization_steps = self.hparams.num_train_epochs * len(
+            self.train_dataloader()
+        )  # Quick Dirty Fix
+        lr_scheduler = {
+            "scheduler": get_linear_schedule_with_warmup(
+                optimizer,
+                num_warmup_steps=self.hparams.warmup_steps,
+                num_training_steps=num_train_optimization_steps,
+            ),
+            "name": "learning_rate",
+            "interval": "step",
+            "frequency": 1,
+        }
+
         return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
 
     def train_dataloader(self):
-        return DataLoader(self.trainset,batch_size=8,shuffle=True)
+        return DataLoader(self.trainset, batch_size=8, shuffle=True)
 
     def val_dataloader(self):
-        return DataLoader(self.validset,batch_size=4,shuffle=True)
+        return DataLoader(self.validset, batch_size=4, shuffle=True)
 
     def test_dataloader(self):
-        return DataLoader(self.testset,batch_size=4,shuffle=True)
+        return DataLoader(self.testset, batch_size=4, shuffle=True)
+
 
 model = CodeT5()
 
-wandb_logger = WandbLogger(name='args.run_name', project='FlashFill')
+wandb_logger = WandbLogger(name="args.run_name", project="FlashFill")
 early_stop_callback = EarlyStopping(
-    monitor='validation_loss',
-    patience=3,
-    strict=False,
-    verbose=False,
-    mode='min'
+    monitor="validation_loss", patience=3, strict=False, verbose=False, mode="min"
 )
-lr_monitor = LearningRateMonitor(logging_interval='step')
+lr_monitor = LearningRateMonitor(logging_interval="step")
 
-trainer = Trainer(gpus=1, 
-                  default_root_dir=os.path.join(args.dir,args.run_name,"runs"), 
-                  logger=wandb_logger, 
-                  callbacks=[early_stop_callback, lr_monitor])
+trainer = Trainer(
+    gpus=1,
+    default_root_dir=os.path.join(args.dir, args.run_name, "runs"),
+    logger=wandb_logger,
+    callbacks=[early_stop_callback, lr_monitor],
+)
 trainer.fit(model)
-model.model.save_pretrained(os.path.join(args.dir,args.run_name,"model"))
+model.model.save_pretrained(os.path.join(args.dir, args.run_name, "model"))
